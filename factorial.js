b3d3a6a9f614c640a8291393b01216c9155b7bba
teejay128 <104542152+Teejay128@users.noreply.github.com>
2022-09-25 18:52:40 +0100

    Added invalid number error message, and other cosmetics

--- factorial.js
+++ factorial.js
@@ -1,6 +1,11 @@
 let button = document.querySelector('button');
+let input = document.querySelector('.input-group')
+let danger = document.querySelector('.has-danger');
+let dangerText = document.querySelector('.invalid-feedback');
 
+document.addEventListener('DOMContentLoaded', () => {danger.style.display = 'none'})
 button.addEventListener('click', generateFactorial);
+danger.addEventListener('click', revert);
 
 function generateFactorial(e){
     e.preventDefault();
@@ -9,11 +14,11 @@ function generateFactorial(e){
     number = parseInt(number);
 
     if(number < 0){
-        console.log('less than 0')
+        invalidInput(`${number} is negative, and i don't do negativity`)
     } else if(number == 0){
-        console.log('is 0')
+        invalidInput(`The factorial of 0 is 1`)
     } else if(number > 100){
-        console.log(`${number} is greater than 100`)
+        invalidInput(`${number} is greater than 100, and 100 is my limit`)
     } else if(number > 0){
         let factArr = [];
         let factorial = 1;
@@ -21,11 +26,22 @@ function generateFactorial(e){
             factArr.unshift(i);
             factorial *= i;
         };
-        let text = "1";
-        for(let i = 2; i <= factArr.length; i++){
-            text += ` * ${i}`
-        };
+        let text;
+        if(number > 10){
+        text = "1 * 2 * 3 * ...";
 
+            for(let i = number-3; i <= factArr.length; i++){
+                text += ` * ${i}`
+            };
+            // console.log(text);
+        } else {
+        text = "1";
+
+            for(let i = 2; i <= factArr.length; i++){
+                text += ` * ${i}`
+            };
+            // console.log(text);
+        }
         let tableBody = document.querySelector('tbody');
 
         tableBody.innerHTML += `
@@ -35,9 +51,20 @@ function generateFactorial(e){
             <td>${factorial}</td>
         </tr>
         `
+        numberInput.value = ""
     } else {
-        console.log('is invalid')
+        invalidInput(`The inputed number is not a valid number`);
     }
 
 }
 
+function invalidInput(errorMessage){
+    input.style.display = "none"
+    danger.style.display = "block"
+    dangerText.textContent = errorMessage
+}
+
+function revert() {
+    input.style.display = "flex"
+    danger.style.display = "none"
+}
